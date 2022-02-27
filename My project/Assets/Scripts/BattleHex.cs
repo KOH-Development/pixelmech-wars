0e7d12e83baa6c1f486d3a8bada97bdf948563a0
Rovendoug <rovendoug@gmail.com>
2022-02-27 14:46:45 -0600

    Over commit to continue work on laptop.

---
+++ My project/Assets/Scripts/BattleHex.cs
@@ -0,0 +1,25 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BattleHex : MonoBehaviour
+{
+    // Start is called before the first frame update
+
+    [SerializeField]
+    int horizontalCoordinate;
+    
+    [SerializeField]
+    int verticalCoordinate;
+
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
